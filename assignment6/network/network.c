2c28e528d4c8e57eafadee1777df6e3c75c5a161
Vishal Gaurav <vishal.gaurav@hotmail.com>
2016-05-11 23:36:48 -0400

    backing up network.c

--- assignment6/network/network.c
+++ assignment6/network/network.c
@@ -84,6 +84,17 @@ int getListeningSocketFD(int port, int maxConn){
     fflush(stdout);
     return conn_listen_fd;
 }
+int getNextHop(int destNodeID){
+	int result = -1 ; // error return value
+	if(routingtable){
+		pthread_mutex_lock(routingtable_mutex);
+		result  = routingtable_getnextnode(routingtable, destNodeID);
+		pthread_mutex_unlock(routingtable_mutex);
+	}else{
+		printf("routing table not initialized\n");
+	}
+	return result;
+}
 //This function is used to for the SNP process to connect to the local ON process on port OVERLAY_PORT.
 //TCP descriptor is returned if success, otherwise return -1.
 int connectToOverlay() { 
@@ -139,6 +150,60 @@ void* routeupdate_daemon(void* arg) {
 	pthread_exit(NULL);
 	return 0;
 }
+/**
+//route update packet definition
+//for a route update packet, the route update information will be stored in the data field of a packet 
+
+//a route update entry
+typedef struct routeupdate_entry {
+        unsigned int nodeID;	//destination nodeID
+        unsigned int cost;	//link cost from the source node(src_nodeID in header) to destination node
+} routeupdate_entry_t;
+
+//route update packet format
+typedef struct pktrt{
+        unsigned int entryNum;	//number of entries contained in this route update packet
+        routeupdate_entry_t entry[MAX_NODE_NUM];
+} pkt_routeupdate_t;
+
+*/
+//If this packet is an Route Update packet, update the distance vector table and the routing table. 
+void handle_route_update_packet(snp_pkt_t *received_packet){
+	pkt_routeupdate_t *route_packet = (pkt_routeupdate_t *)malloc(sizeof(pkt_routeupdate_t));
+	memcpy(route_packet,received_packet->data,received_packet->header.length);
+	int srcNodeID = received_packet->header.src_nodeID;
+	int totalRoutes = route_packet->entryNum;
+	int total_neighbours = topology_getNbrNum(); // this is needed to update dv entries of my node
+	for(int index = 0 ; index < totalRoutes ; index++){
+		pthread_mutex_lock(dv_mutex);
+		dvtable_setcost(dv,srcNodeID,route_packet->entry[index].nodeID,route_packet->entry[index].cost);
+		pthread_mutex_unlock(dv_mutex);
+	}
+	free(route_packet);
+}
+
+//If the packet is a SNP packet and the destination node is this node, forward the packet to the SRT process.
+//If the packet is a SNP packet and the destination node is not this node, forward the packet to the next hop according to the routing table.
+void handle_snp_packet(snp_pkt_t *received_packet){
+	int myNodeId = topology_getMyNodeID();
+	if(received_packet->header.dest_nodeID == myNodeId){
+		seg_t *received_seg = (seg_t *) malloc(sizeof(seg_t));
+		// forward packet to SRT
+		//forwardsegToSRT(int tran_conn, int src_nodeID, seg_t* segPtr)
+		memcpy(received_seg, received_packet->data, received_packet->header.length);
+		if(forwardsegToSRT(transport_conn,received_packet->header.src_nodeID,received_seg) < 0 ){
+			printf("handle_snp_packet : forwardsegToSRT ERROR\n");
+		}
+		free(received_seg);
+	}else{
+		int nextHop = getNextHop(received_packet->header.dest_nodeID);
+		if(!overlay_sendpkt(nextHop,received_packet,overlay_conn))
+		{
+			printf("handle_snp_packet : overlay_sendpkt ERROR nextHop = %d, destNode = %d \n", nextHop, received_packet->header.dest_nodeID);
+		}
+
+	}
+}
 
 //This thread handles incoming packets from the ON process.
 //It receives packets from the ON process by calling overlay_recvpkt().
@@ -146,10 +211,19 @@ void* routeupdate_daemon(void* arg) {
 //If the packet is a SNP packet and the destination node is not this node, forward the packet to the next hop according to the routing table.
 //If this packet is an Route Update packet, update the distance vector table and the routing table. 
 void* pkthandler(void* arg) {
-	snp_pkt_t pkt;
-	bzero(&pkt,sizeof(pkt));
-	while(overlay_recvpkt(&pkt,overlay_conn)>0) {
-		printf("Routing: received a packet in node %d from neighbor %d\n", topology_getMyNodeID(), pkt.header.src_nodeID);
+	snp_pkt_t received_packet;
+	bzero(&received_packet,sizeof(received_packet));
+	while(overlay_recvpkt(&received_packet,overlay_conn)>0) {
+		printf("Routing: received a packet in node %d from neighbor %d\n", topology_getMyNodeID(), received_packet.header.src_nodeID);
+		if(received_packet.header.type == ROUTE_UPDATE){
+			printf("ROUTE_UPDATE packet recieved\n");
+			handle_route_update_packet(&received_packet);
+		}else if(received_packet.header.type == SNP){
+			printf("SNP packet received\n");
+			handle_snp_packet(&received_packet);
+		}else{
+			printf("pktHandler packet dropped, type not identified\n");
+		}
 	}
 	close(overlay_conn);
 	printf("Routing: Packet handler thread STOPPED\n");
@@ -163,25 +237,94 @@ void* pkthandler(void* arg) {
 void network_stop() {
 	//add your code here
 	close(overlay_conn);
+	close(transport_conn);
+	routingtable_destroy(routingtable);
+	nbrcosttable_destroy(nct);
+	dvtable_destroy(dv);
 	printf(" network_stop called :- SNP process stopped\n Exiting .. !! \n");
 	exit(0);
 }
-//After the local SRT process is connected, this function keeps receiving sendseg_arg_ts which contains the segments and their destination node addresses from the SRT process. The received segments are then encapsulated into packets (one segment in one packet), and sent to the next hop using overlay_sendpkt. The next hop is retrieved from routing table.
+/*
+//segment header definition. 
+
+typedef struct srt_hdr {
+	unsigned int src_port;        //source port number
+	unsigned int dest_port;       //destination port number
+	unsigned int seq_num;         //sequence number
+	unsigned int ack_num;         //ack number
+	unsigned short int length;    //segment data length
+	unsigned short int  type;     //segment type
+	unsigned short int  rcv_win;  //currently not used
+	unsigned short int checksum;  //checksum for this segment
+} srt_hdr_t;
+
+//segment definition
+
+typedef struct segment {
+	srt_hdr_t header;
+	char data[MAX_SEG_LEN];
+} seg_t;
+
+*/
+/*
+//SNP packet format definition
+typedef struct snpheader {
+  int src_nodeID;		          //source node ID
+  int dest_nodeID;		        //destination node ID
+  unsigned short int length;	//length of the data in the packet
+  unsigned short int type;	  //type of the packet 
+} snp_hdr_t;
+
+typedef struct packet {
+  snp_hdr_t header;
+  char data[MAX_PKT_LEN];
+} snp_pkt_t;
+*/
+
+void forwardPacketToNextNode(snp_pkt_t *received_packet, seg_t *segPtr, int *destNodeID){
+	if(received_packet && segPtr && destNodeID)
+	{
+		printf("forwarding packet to \n");
+		// wrap segment into packet
+		received_packet->header.src_nodeID = topology_getMyNodeID();
+		received_packet->header.type = SNP;
+		received_packet->header.dest_nodeID = *destNodeID;
+		received_packet->header.length = sizeof(seg_t) + segPtr->header.length;
+		int nextHop = getNextHop(*destNodeID);
+		printf("nextHop for destNode %d is %d \n", *destNodeID, nextHop );
+		// packet char array will contain all the values of seg_t which means memcpy seg_t to packet->data
+		memcpy(received_packet->data, segPtr, sizeof(seg_t) + segPtr->header.length);
+		if(!overlay_sendpkt(nextHop,received_packet,overlay_conn))
+		{
+			printf("forwardPacketToNextNode: overlay send packet error nextHop = %d, destNode = %d \n", nextHop, *destNodeID);
+		}
+	}
+	else
+	{
+		printf("Error in forwardPacketToNextNode \n");
+	}
+}
+//After the local SRT process is connected, this function keeps receiving sendseg_arg_ts which contains the segments and their destination node addresses from the SRT process. 
+//The received segments are then encapsulated into packets (one segment in one packet), and sent to the next hop using overlay_sendpkt. The next hop is retrieved from routing table.
 //When a local SRT process is disconnected, this function waits for the next SRT process to connect.
-void keepReceivingPacketsFromSRT(){
+void keepReceivingSegemtnsFromSRT(){
 	printf("keepReceivingPacketsFromSRT starts \n");
 	snp_pkt_t *received_packet = (snp_pkt_t *)malloc(sizeof(snp_pkt_t));
-    int *srcNodeID = (int *)malloc(sizeof(int));
+    int *destNodeID = (int *)malloc(sizeof(int));
     // recieve the segment 
     seg_t *segPtr = (seg_t *)malloc(sizeof(seg_t));
+    bzero(segPtr,sizeof(seg_t));
+    bzero(received_packet,sizeof(snp_pkt_t));
     printf("SRT LAYER STARTED\n");
-    while (getsegToSend(transport_conn, srcNodeID, segPtr) > 0) {
-    	printf("packet received from SRT node id = %d\n", *srcNodeID);
+    while (getsegToSend(transport_conn, destNodeID, segPtr) > 0) {
+    	printf("segment received from SRT node id = %d\n", *destNodeID);
+    	forwardPacketToNextNode(received_packet,segPtr,destNodeID);
     	fflush(stdout);
     }
 	printf("SRT packet receiving ended for SRT socket %d \n",transport_conn);
     free(received_packet);
-    free(srcNodeID);
+    free(segPtr);
+    free(destNodeID);
     close(transport_conn);
     transport_conn = -1;
     printf("keepReceivingPacketsFromSRT ends \n");
@@ -206,15 +349,15 @@ void waitTransport() {
 	socklen_t client_addr_length  = sizeof(node_client_addr);
 	while(1){
 		bzero(&node_client_addr,sizeof(node_client_addr));
-		printf("Waiting for SNP process to connect\n");
+		printf("Waiting for SRT process to connect\n");
 	    transport_conn = accept(snp_server_fd,(struct sockaddr*)&node_client_addr,&client_addr_length);	
 		if(transport_conn < 0){
-			printf("error in accepting SNP connection return %d\n Exiting program !! \n",transport_conn );
+			printf("error in accepting SRT connection return %d\n Exiting program !! \n",transport_conn );
 		}else{
 			char ipaddress[100]; 
     		inet_ntop( AF_INET, &(node_client_addr.sin_addr), ipaddress, INET_ADDRSTRLEN );
-			printf("SNP process connected with conn fd %d and ip address %s \n", transport_conn, ipaddress );
-			keepReceivingPacketsFromSRT();
+			printf("SRT process connected with conn fd %d and ip address %s \n", transport_conn, ipaddress );
+			keepReceivingSegemtnsFromSRT();
 		}
 		sleep(1); // wait for 1 sec to get new SNP connection
 	}
